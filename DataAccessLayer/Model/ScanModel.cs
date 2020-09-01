cbe0385349e91752413a9e1a268cec99bfc846a0
Jason Yuan <jason_yuan@epam.com>
2020-09-01 23:30:07 +0800

    first commit

---
+++ DataAccessLayer/Model/ScanModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayer.Model
+{
+    public class ScanModel
+    {
+        public string DeviceId { get; set; }
+        public string ModelNumber { get; set; }
+        public string HardwareVersion { get; set; }
+        public string QRCodeFormat { get; set; }
+        public string EncryptedQRCode { get; set; }
+        public string ScanTime { get; set; }
+    }
+}
